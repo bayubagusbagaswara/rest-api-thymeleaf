08840eef2de47eb28062b2a6894219ed9add9e49
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-11-24 20:32:14 +0700

    Initial commit

---
+++ src/test/java/com/bayu/rest/api/thymeleaf/RestApiThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayu.rest.api.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
