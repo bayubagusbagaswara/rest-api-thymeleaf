2370b891380622de210453b92ffdcd357e83eb40
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-11-25 20:57:25 +0700

    add entity Product

---
+++ src/main/java/com/bayu/rest/api/thymeleaf/entity/Product.java
@@ -0,0 +1,28 @@
+package com.bayu.rest.api.thymeleaf.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "PRODUCTS")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "PRICE")
+    private BigDecimal price;
+}
